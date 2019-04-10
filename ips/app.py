a948676e6d8b1372e184b228054382714009c110
Paul Soule <paul.soule@ext.ons.gov.uk>
2019-04-10 14:30:14 +0100

    Export endpoint added

--- ips/app.py
+++ ips/app.py
@@ -28,66 +28,42 @@ workflow = ips_workflow.IPSWorkflow()
 
 app = falcon.API(middleware=MultipartMiddleware())
 
-status_api = StatusApi(workflow)
-start_api = StartApi(workflow)
-cancel_api = CancelApi(workflow)
-apply_pvs = ApplyPVsApi(workflow)
-run_api = RunApi(workflow)
-run_steps_api = RunStepsApi(workflow)
-run_steps_value_api = RunStepsValueApi(workflow)
-run_steps_value_steps_api = RunStepsValueStepApi(workflow)
-
-pv_sets_api = PvSetsApi(workflow)
-
-login_api = LoginApi(workflow)
-
 log.setLevel(logging.DEBUG)
 
-pv_api = PvApi(workflow)
-pv_builder_api = PvBuilderApi(workflow)
-pv_builder_variables_api = PvBuilderVariablesApi(workflow)
+app.add_route('/ips-service/start/{run_id}', StartApi(workflow))
+app.add_route('/ips-service/status/{run_id}', StatusApi(workflow))
+app.add_route('/ips-service/cancel/{run_id}', CancelApi(workflow))
 
-import_survey = ImportSurvey(workflow)
-import_air = ImportAir(workflow)
-import_non_response = ImportNonResponse(workflow)
-import_sea = ImportSea(workflow)
-import_shift = ImportShift(workflow)
-import_tunnel = ImportTunnel(workflow)
-import_unsampled = ImportUnsampled(workflow)
+app.add_route("/runs", RunApi(workflow))
 
-export_api = ExportApi(workflow)
+app.add_route("/run_steps/{run_id}", RunStepsApi(workflow))
+app.add_route("/run_steps/{run_id}/{value}", RunStepsValueApi(workflow))
+app.add_route("/run_steps/{run_id}/{value}/{step_number}", RunStepsValueStepApi(workflow))
 
-app.add_route('/ips-service/start/{run_id}', start_api)
-app.add_route('/ips-service/status/{run_id}', status_api)
-app.add_route('/ips-service/cancel/{run_id}', cancel_api)
+app.add_route("/pv_sets", PvSetsApi(workflow))
 
-app.add_route("/runs", run_api)
+app.add_route("/login/{user_name}/{password}", LoginApi(workflow))
 
-app.add_route("/run_steps/{run_id}", run_steps_api)
-app.add_route("/run_steps/{run_id}/{value}", run_steps_value_api)
-app.add_route("/run_steps/{run_id}/{value}/{step_number}", run_steps_value_steps_api)
+app.add_route("/process_variables/{run_id}", PvApi(workflow))
 
-app.add_route("/pv_sets", pv_sets_api)
+app.add_route("/builder/{run_id}", PvBuilderApi(workflow))
+app.add_route("/builder/{run_id}/{pv_id}", PvBuilderApi(workflow))
+app.add_route("/builder/variables", PvBuilderVariablesApi(workflow))
 
-app.add_route("/login/{user_name}/{password}", login_api)
+app.add_route("/import/survey/{run_id}", ImportSurvey(workflow))
+app.add_route("/import/air/{run_id}", ImportAir(workflow))
+app.add_route("/import/nonresponse/{run_id}", ImportNonResponse(workflow))
+app.add_route("/import/sea/{run_id}", ImportSea(workflow))
+app.add_route("/import/shift/{run_id}", ImportShift(workflow))
+app.add_route("/import/tunnel/{run_id}", ImportTunnel(workflow))
+app.add_route("/import/unsampled/{run_id}", ImportUnsampled(workflow))
 
-app.add_route("/process_variables/{run_id}", pv_api)
+app.add_route("/applypvs/{run_id}/{pv_set}", ApplyPVsApi(workflow))
 
-app.add_route("/builder/{run_id}", pv_builder_api)
-app.add_route("/builder/{run_id}/{pv_id}", pv_builder_api)
-app.add_route("/builder/variables", pv_builder_variables_api)
 
-app.add_route("/import/survey/{run_id}", import_survey)
-app.add_route("/import/air/{run_id}", import_air)
-app.add_route("/import/nonresponse/{run_id}", import_non_response)
-app.add_route("/import/sea/{run_id}", import_sea)
-app.add_route("/import/shift/{run_id}", import_shift)
-app.add_route("/import/tunnel/{run_id}", import_tunnel)
-app.add_route("/import/unsampled/{run_id}", import_unsampled)
+app.add_route("/applypvs/{run_id}/{pv_set}", ApplyPVsApi(workflow))
 
-app.add_route("/applypvs/{run_id}/{pv_set}", apply_pvs)
-
-app.add_route("/export/{run_id}/{table_name}", export_api)
+app.add_route("/export/{run_id}/{table_name}", ExportApi(workflow))
 
 from waitress import serve
 
