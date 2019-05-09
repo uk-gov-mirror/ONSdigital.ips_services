0d9a180c41b1c34707dcdee13244ad9d98ff0de5
Paul Soule <paul.soule@ext.ons.gov.uk>
2019-05-09 19:16:35 +0100

    Fixed load tests. oops.

--- tests/test_load_traffic.py
+++ tests/test_load_traffic.py
@@ -3,7 +3,7 @@ import uuid
 
 import ips_common_db.sql as db
 from ips_common.ips_logging import log
-from ips.services.dataimport.import_traffic import import_air_file, import_sea_file, import_tunnel_file
+from ips.services.dataimport.import_traffic import import_air, import_sea, import_tunnel
 from ips.persistence.import_traffic import TRAFFIC_TABLE
 
 tunnel_data = "data/import_data/dec/Tunnel Traffic Dec 2017.csv"
@@ -14,18 +14,23 @@ run_id = str(uuid.uuid4())
 start_time = time.time()
 
 
+# noinspection PyUnusedLocal
 def setup_module(module):
     log.info("Module level start time: {}".format(start_time))
 
 
 def test_survey_import():
     log.info(f"-> Start traffic data load for run_id: {run_id}")
-    air_df = import_air_file(run_id, air_data)
-    tunnel_df = import_tunnel_file(run_id, tunnel_data)
-    sea_df = import_sea_file(run_id, sea_data)
+    with open(air_data, 'rb') as file:
+        air_df = import_air(run_id, file.read())
+    with open(tunnel_data, 'rb') as file:
+        tunnel_df = import_tunnel(run_id, file.read())
+    with open(sea_data, 'rb') as file:
+        sea_df = import_sea(run_id, file.read())
     log.info(f"-> End traffic data load. air: {len(air_df)} rows, tunnel: {len(tunnel_df)}, sea: {len(sea_df)}.")
 
 
+# noinspection PyUnusedLocal
 def teardown_module(module):
     log.info("Duration: {}".format(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))))
     db.delete_from_table(TRAFFIC_TABLE, 'RUN_ID', '=', run_id)
--- tests/test_load_unsampled.py
+++ tests/test_load_unsampled.py
@@ -5,7 +5,7 @@ import ips_common_db.sql as db
 from ips_common.ips_logging import log
 
 from ips.persistence.import_unsampled import UNSAMPLED_OOH_DATA
-from ips.services.dataimport.import_unsampled import import_unsampled_file
+from ips.services.dataimport.import_unsampled import import_unsampled
 
 unsampled_data = "data/import_data/dec/Unsampled Traffic Dec 2017.csv"
 
@@ -13,16 +13,19 @@ run_id = str(uuid.uuid4())
 start_time = time.time()
 
 
+# noinspection PyUnusedLocal
 def setup_module(module):
     log.info("Module level start time: {}".format(start_time))
 
 
 def test_survey_import():
     log.info(f"-> Start unsampled data load for run_id: {run_id}")
-    df = import_unsampled_file(run_id, unsampled_data)
+    with open(unsampled_data, 'rb') as file:
+        df = import_unsampled(run_id, file.read())
     log.info(f"-> End unsampled data load. {len(df)} rows.")
 
 
+# noinspection PyUnusedLocal
 def teardown_module(module):
     log.info("Duration: {}".format(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))))
     db.delete_from_table(UNSAMPLED_OOH_DATA, 'RUN_ID', '=', run_id)
