9f5bdab9c09f8ec4bd6b6420a903d04ffe597687
Paul Soule <22938608+pds2208@users.noreply.github.com>
2019-05-10 10:57:01 +0100

    Removed all deprecated and truncated messages New database schema - UPDATE! Added run management to keep track of run status Removed file upload - load all from stream Simplified API calls
    Survey validation & tests
    
    Ready to merge

--- tests/test_load_survey.py
+++ tests/test_load_survey.py
@@ -1,11 +1,14 @@
 import time
 import uuid
 
+import falcon
 import ips_common_db.sql as db
 from ips_common.ips_logging import log
 
 from ips.persistence.import_survey import SURVEY_SUBSAMPLE
 from ips.services.dataimport.import_survey import import_survey
+
+import pytest
 
 survey_data = "data/import_data/dec/surveydata.csv"
 
@@ -18,11 +21,43 @@ def setup_module(module):
     log.info("Module level start time: {}".format(start_time))
 
 
-def test_survey_import():
-    log.info(f"-> Start survey data load for run_id: {run_id}")
+def test_month1():
     with open(survey_data, 'rb') as file:
-        df = import_survey(run_id, file.read(), None, None)
-    log.info(f"-> End survey data load. {len(df)} rows.")
+        with pytest.raises(falcon.HTTPError) as e_info:
+            import_survey(run_id, file.read(), '25', '2009')
+
+
+def test_month2():
+    # matching year, invalid month
+    with open(survey_data, 'rb') as file:
+        with pytest.raises(falcon.HTTPError) as e_info:
+            import_survey(run_id, file.read(), '11', '2017')
+
+
+def test_year1():
+    # valid month, data not matching valid year
+    with open(survey_data, 'rb') as file:
+        with pytest.raises(falcon.HTTPError) as e_info:
+            import_survey(run_id, file.read(), '12', '2009')
+
+
+def test_invalid_quarter():
+    # valid month, data not matching valid year
+    with open(survey_data, 'rb') as file:
+        with pytest.raises(falcon.HTTPError) as e_info:
+            import_survey(run_id, file.read(), 'Q5', '2009')
+
+
+def test_valid_quarter():
+    # valid month, data not matching valid year
+    with open(survey_data, 'rb') as file:
+        with pytest.raises(falcon.HTTPError) as e_info:
+            import_survey(run_id, file.read(), 'Q4', '2017')
+
+
+def test_valid_import():
+    with open(survey_data, 'rb') as file:
+        import_survey(run_id, file.read(), '12', '2017')
 
 
 # noinspection PyUnusedLocal
