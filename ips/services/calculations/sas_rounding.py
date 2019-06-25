082319af2229313c5d101259b8bb297c16ab0918
Paul Soule <22938608+pds2208@users.noreply.github.com>
2019-06-25 11:51:58 +0100

    Fare imputation - working!! configurable rounding function sas_random in a separate class

--- ips/services/calculations/sas_rounding.py
+++ ips/services/calculations/sas_rounding.py
@@ -1,11 +1,14 @@
 import math
 import pandas as pd
 
-
 # This needs to be in a separate file so that it can be accessed by the configuration module
 
 # use SAS style rounding
-def sas_round(n, decimals=0):
+from ips.util.services_configuration import ServicesConfiguration
+from ips.util.services_logging import log
+
+
+def _sas_round(n, decimals=0):
     if not isinstance(n, (float, int)):
         fare = pd.to_numeric(n, errors="coerce")
     else:
@@ -17,3 +20,13 @@ def sas_round(n, decimals=0):
     multiplier = 10 ** decimals
 
     return math.floor(n * multiplier + 0.5) / multiplier
+
+
+# set to python default rounding unless overridden in configuration
+ips_rounding = round
+
+if ServicesConfiguration().sas_rounding():
+    log.debug("Selecting SAS style rounding")
+    ips_rounding = _sas_round
+else:
+    log.debug("Selecting Python style rounding")
