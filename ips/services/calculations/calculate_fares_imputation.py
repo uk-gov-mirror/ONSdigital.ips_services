1f958cbbad6ca529415dc014048b4d83e005b04e
Andy Urquhart <35800749+urquha@users.noreply.github.com>
2019-08-14 14:31:37 +0100

    cli test change

--- ips/services/calculations/calculate_fares_imputation.py
+++ ips/services/calculations/calculate_fares_imputation.py
@@ -8,7 +8,7 @@ from ips.services.calculations.sas_random import SASRandom
 from ips.services.calculations.sas_rounding import ips_rounding
 
 from ips.util.services_logging import log
-
+# changesssssssssssssssssssss
 # dataimport survey_support
 
 INTDATE = 'INTDATE'
