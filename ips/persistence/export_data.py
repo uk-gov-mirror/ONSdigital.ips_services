75db915a9739e3dd2c59710f85b566e017754528
Andy Urquhart <35800749+urquha@users.noreply.github.com>
2019-11-04 13:51:42 +0000

    Replaced -1's with nan and dropped RunID from survey data export

--- ips/persistence/export_data.py
+++ ips/persistence/export_data.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pandas as pd
+import numpy as np
 from ips.util.services_logging import log
 
 from ips.persistence.persistence import select_data, insert_from_dataframe
@@ -12,6 +13,9 @@ insert_downloads = insert_from_dataframe(EXPORT_DATA_DOWNLOAD, "append")
 
 def export_from_table(run_id, table):
     df = select_data(column_name="*", table=table, condition1="RUN_ID", condition2=run_id)
+    df.replace(to_replace=-1, value=np.nan, inplace=True)
+    if table == "SURVEY_SUBSAMPLE":
+        df = df.drop(columns=['RUN_ID'])
     csv = df.to_csv(index=False)
     return create_export_data_download(run_id, table, csv)
 
