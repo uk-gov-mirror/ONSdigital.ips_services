46df61f00d48137e56a434c338b34c2d690d3505
elthorne <elinor.thorne@ons.gov.uk>
2020-04-08 11:51:07 +0100

    Removed unnecessary code

--- ips/persistence/pv_builder_persistence.py
+++ ips/persistence/pv_builder_persistence.py
@@ -1,8 +1,5 @@
-import json
 from ips.util.services_logging import log
-
-from ips.persistence.persistence import delete_from_table, execute_sql, get_identity, \
-    insert_into_table, insert_into_table_id, read_table_values
+from ips.persistence.persistence import delete_from_table, execute_sql, insert_into_table
 
 PV_BYTES = 'PROCESS_VARIABLE_PY'
 
